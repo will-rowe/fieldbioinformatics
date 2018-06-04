92343f55b03af04069de31351721bfd01362ce66
Nick Loman <n.j.loman@bham.ac.uk>
2018-06-04 15:54:24 +0100

    remove old shell scripts

--- artic/align.sh
+++
@@ -1,1 +0,0 @@
-align6mer.sh
--- artic/illumina_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sample=$1
-fq1=$1
-fq2=$2
-scheme=$3
-
-snakemake -p --verbose --config scheme="${scheme}" fq1="${fq1}" fq2="${fq2}" sample="${sample}"  --snakefile /zibra/zika-pipeline/scripts/illumina/align_reads/Snakefile all
-
