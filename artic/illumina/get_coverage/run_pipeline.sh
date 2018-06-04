5277f655032ba7f9b06c356fa0eeed27b7f0c375
Nick Loman <n.j.loman@bham.ac.uk>
2018-06-04 15:55:03 +0100

    remove zibra illumina pipeline

--- artic/illumina/get_coverage/run_pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#PBS -l walltime=120:00:00 -l nodes=1 -q new -o /gpfs/home/gkarthik/logs/new.txt -j oe
-
-snakemake
-# If executing on a cluster
-# snakemake -j 50 --cluster-config cluster.json --cluster "qsub -l walltime={cluster.walltime} -l mem={cluster.mem} -l nodes={cluster.n} -q {cluster.queue} -o {cluster.logfile} -j {cluster.stdout}"
