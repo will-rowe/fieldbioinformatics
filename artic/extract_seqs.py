1cc0fd156354c0fa7161987f4820ec01215ebf49
Nick Loman <n.j.loman@bham.ac.uk>
2018-06-09 11:26:35 +0100

    tidy up

--- artic/extract_seqs.py
+++ artic/extract_seqs.py
@@ -2,6 +2,6 @@
 from Bio import SeqIO
 import sys
 
-for rec in SeqIO.parse(sys.stdin, "fasta"):
-	if rec.id in sys.argv[1:]:
-		SeqIO.write([rec], sys.stdout, "fasta")
+for rec in SeqIO.parse(sys.argv[1], "fastq"):
+	if rec.id in sys.argv[2:]:
+		SeqIO.write([rec], sys.stdout, "fastq")
