b508a2e3e34b8b622b76a0aa898266df3a2b0c4b
Nick Loman <n.j.loman@bham.ac.uk>
2018-12-08 12:29:01 +0000

    clarify help

--- artic/align_trim.py
+++ artic/align_trim.py
@@ -189,7 +189,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='Trim alignments from an amplicon scheme.')
     parser.add_argument('bedfile', help='BED file containing the amplicon scheme')
-    parser.add_argument('--normalise', type=int, help='Subsample to n coverage')
+    parser.add_argument('--normalise', type=int, help='Subsample to n coverage per strand')
     parser.add_argument('--report', type=str, help='Output report to file')
     parser.add_argument('--start', action='store_true', help='Trim to start of primers instead of ends')
     parser.add_argument('--verbose', action='store_true', help='Debug mode')
