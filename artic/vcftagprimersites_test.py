b6797f22222bc01e0b758f8a2b039ea7d61e2ba6
Will Rowe <w.p.m.rowe@gmail.com>
2020-03-24 16:07:06 +0000

    adding travis

--- artic/vcftagprimersites_test.py
+++ artic/vcftagprimersites_test.py
@@ -1,14 +1,18 @@
 # test_vcftagprimersites.py is the unit tests for the vcf primer site tagging
 import pytest
+import os
 
 from artic import vcftagprimersites
+
+# pytest needs something like this to resolve where test data is kept:
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_read_bed_file():
 
     # process the nCoV-2019 primer scheme
     bedfile = vcftagprimersites.read_bed_file(
-        "../test-data/nCoV-2019.scheme.bed")
+        TEST_DIR + "/../test-data/nCoV-2019.scheme.bed")
 
     # check the the alts have been collapsed into a canonical primer site
     assert len(bedfile) == 196, "alts where not collapsed"
