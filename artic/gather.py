f84b2b797825110a4e7ea838990336818aa15215
Nick Loman <n.j.loman@bham.ac.uk>
2020-03-26 17:05:23 +0000

    put back sagrudd gather fix

--- artic/gather.py
+++ artic/gather.py
@@ -83,7 +83,7 @@ def run(parser, args):
             barcode_directory = paths[-1]
 
             fastq[barcode_directory].extend([root+'/'+f for f in files if f.endswith('.fastq')])
-            summary_files.extend([root+'/'+f for f in files if f.find('_summary.txt') != -1])
+            summary_files.extend([root+'/'+f for f in files if fnmatch.fnmatch(f, '*cing_summary*txt')])
 
     for barcode_directory, fastq in list(fastq.items()):
         if len(fastq):
